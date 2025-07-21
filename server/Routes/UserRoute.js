cccc2736fb7f6c0465d1c072c9b886efd6a78176
GenZGalazytech <genzgalaxytech@gmail.com>
2025-07-21 15:32:50 +0530

    Update UserRoute.js

--- server/Routes/UserRoute.js
+++ server/Routes/UserRoute.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const {
   getSomedata,
   getdatabyid
-} = require('../controllers/UserController');
+} = require('../Controllers/UserController');
 
 router.get('/', getSomedata);
 router.get('/:id', getdatabyid);
